64077061579617d3273e2237973bcc5aabd6ec6a
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2022-01-14 20:00:30 +0100

    Haddock documentation for Data.BuffonMachine

--- src/Data/BuffonMachine.hs
+++ src/Data/BuffonMachine.hs
@@ -1,13 +1,25 @@
 -- |
 -- Module      : Data.BuffonMachine
--- Description :
+-- Description : Buffon machines providing random variates for discrete probability distributions.
 -- Copyright   : (c) Maciej Bendkowski, 2022
 -- License     : BSD3
 -- Maintainer  : maciej.bendkowski@gmail.com
 -- Stability   : experimental
+--
+-- Monad for computations consuming random bits provided by a buffered random
+-- bit oracle.
+--
+-- References:
+--
+-- * Ph. Flajolet, M. Pelletier, M. Soria : "On Buffon Machines and Numbers", SODA'11 (2011).
+-- * J. Lumbroso : "Optimal Discrete Uniform Generation from Coin Flips, and Applications".
+-- * F. A. Saad, C. E. Freer, M. C. Rinard, V.K. Mansinghka "Optimal Approximate
+--     Sampling from Discrete Probability Distributions", POPL'20 (2020).
 module Data.BuffonMachine
   ( BuffonMachine,
     Discrete,
+    Oracle (..),
+    DDG,
     choice,
     run,
     runIO,
@@ -27,9 +39,13 @@ import Data.Word (Word32)
 import System.Random (Random (random), RandomGen, StdGen, getStdGen)
 import Prelude hiding (null)
 
+-- | Buffered random bit oracle.
 data Oracle g = Oracle
-  { buffer :: !Word32,
+  { -- | 32-bit buffer of random bits.
+    buffer :: !Word32,
+    -- | Number of bits consumed from the current buffer.
     usedBits :: !Int,
+    -- | Random number generator used to obtain random bits.
     rng :: g
   }
 
@@ -46,7 +62,8 @@ currentBit :: Oracle g -> Bool
 currentBit oracle = testBit (buffer oracle) (usedBits oracle)
 {-# INLINE currentBit #-}
 
--- | Buffon machines depending on the given random number generator 'g'.
+-- |
+--  Buffon machines implemented as a `State` monad over `Oracle`.
 type BuffonMachine g = State (Oracle g)
 
 type Bern g = BuffonMachine g Bool
@@ -65,19 +82,25 @@ getBit = do
   put $ useBit oracle
   return $ currentBit oracle
 
--- | Discrete random variables.
+-- |
+--  Buffon machine computations resulting in discrete random variables.
 type Discrete g = BuffonMachine g Int
+
+-- |
+--   Discrete distribution generating trees in compact vector form.
+--   Trivial, single-point distributions are encoded as empty vectors.
+type DDG = Vector Int
 
 -- |
 --  Given a compact discrete distribution generating tree (in vector form)
 --  computes a discrete random variable following that distribution.
-choice :: RandomGen g => Vector Int -> Discrete g
+choice :: RandomGen g => DDG -> Discrete g
 choice enc
-  | null enc = return 0 -- note: single-point probability distributions.
+  | null enc = return 0
   | otherwise = choice' enc 0
-{-# SPECIALIZE choice :: Vector Int -> Discrete StdGen #-}
+{-# SPECIALIZE choice :: DDG -> Discrete StdGen #-}
 
-choice' :: RandomGen g => Vector Int -> Int -> Discrete g
+choice' :: RandomGen g => DDG -> Int -> Discrete g
 choice' enc c = do
   h <- getBit
   let b = fromEnum h
@@ -85,13 +108,12 @@ choice' enc c = do
   if enc ! c' < 0 then return $ -(1 + enc ! c') else choice' enc c'
 
 -- |
---  Runs the given Buffon machine computation
---  using the given random generator.
+--  Runs the given Buffon machine computation using the given random generator.
 run :: RandomGen g => BuffonMachine g a -> g -> a
 run m = evalState m . fresh
 
 -- |
---  Runs the given Buffon machine computation within the IO monad
---  using StdGen as its random bit oracle.
+--  Runs the given Buffon machine computation within the IO monad using StdGen
+--  as its random bit generator.
 runIO :: BuffonMachine StdGen a -> IO a
 runIO m = run m <$> getStdGen
