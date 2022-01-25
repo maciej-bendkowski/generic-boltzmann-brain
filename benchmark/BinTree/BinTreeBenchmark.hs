66c047e07080de6a25ec10919e453e4f76cd3f53
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2022-01-25 21:22:12 +0100

    Benchmark bulk generation of binTrees.

--- benchmark/BinTree/BinTreeBenchmark.hs
+++ benchmark/BinTree/BinTreeBenchmark.hs
@@ -1,17 +1,12 @@
 import BinTree (BinTree)
-import BinTreeSampler (randomBinTreeIO)
+import BinTreeSampler (randomBinTreeListIO)
 import Control.Monad (replicateM)
 import Criterion.Main (bench, defaultMain, whnfIO)
 
-sampler :: Int -> Int -> Int -> IO [BinTree]
-sampler n lb ub = replicateM n (randomBinTreeIO lb ub)
+sampler :: Int -> IO [BinTree]
+sampler = randomBinTreeListIO 3600 4400
 
 main :: IO ()
 main =
   defaultMain
-    [ bench "1 binary tree" $ whnfIO (sampler 1 3600 4400)
-    , bench "10 binary trees" $ whnfIO (sampler 10 3600 4400)
-    , bench "100 binary trees" $ whnfIO (sampler 100 3600 4400)
-    , bench "1000 binary trees" $ whnfIO (sampler 1000 3600 4400)
-    , bench "10,000 binary trees" $ whnfIO (sampler 10000 3600 4400)
-    ]
+    [bench "100 binary trees" $ whnfIO (sampler 100)]
--- benchmark/BinTree/BinTreeSampler.hs
+++ benchmark/BinTree/BinTreeSampler.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module BinTreeSampler (randomBinTreeIO) where
+module BinTreeSampler (randomBinTreeListIO) where
 
 import BinTree (BinTree, binTreeSysSpec)
+import Control.Monad (replicateM)
 import Data.Boltzmann.Oracle (mkSpecSampler)
-import Data.Boltzmann.Sampler (rejectionSamplerIO)
+import Data.Boltzmann.Sampler (rejectionSampler)
+import Data.BuffonMachine (runIO)
 
-randomBinTreeIO :: Int -> Int -> IO BinTree
-randomBinTreeIO = rejectionSamplerIO $(mkSpecSampler binTreeSysSpec)
+randomBinTreeListIO :: Int -> Int -> Int -> IO [BinTree]
+randomBinTreeListIO lb ub n =
+  runIO $
+    replicateM n (rejectionSampler $(mkSpecSampler binTreeSysSpec) lb ub)
