e186b51c0b696c20eff6ea8dbd870bd3b384686a
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2022-01-29 19:53:55 +0100

    Evaluate to normal form in benchmarks.

--- benchmark/BinTree/BinTreeBenchmark.hs
+++ benchmark/BinTree/BinTreeBenchmark.hs
@@ -2,8 +2,7 @@
 
 import BinTree (BinTree)
 import BinTreeSampler (mediumRandomBinTreeListIO, randomBinTreeListIO)
-import Control.Monad (replicateM)
-import Criterion.Main (bench, defaultMain, whnfIO)
+import Criterion.Main (bench, defaultMain, nfIO)
 
 sampler :: Int -> IO [BinTree]
 sampler = randomBinTreeListIO 800 1200
@@ -14,6 +13,6 @@ mediumSampler = mediumRandomBinTreeListIO 8_000 12_000
 main :: IO ()
 main =
   defaultMain
-    [ bench "100 binary trees" $ whnfIO (sampler 100)
-    , bench "100 large binary trees" $ whnfIO (mediumSampler 100)
+    [ bench "100 binary trees" $ nfIO (sampler 100)
+    , bench "100 large binary trees" $ nfIO (mediumSampler 100)
     ]
